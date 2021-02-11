dfc1e736d9a1cf3884e61eb5fb082b99299ce7b1
wumamu <wcclevi1225@gmail.com>
2021-02-11 21:34:44 +0800

    21.02.12

--- app_i/src/main/java/com/recoveryrecord/surveyandroid/example/NLService.java
+++ app_i/src/main/java/com/recoveryrecord/surveyandroid/example/NLService.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.text.format.DateFormat;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import androidx.annotation.RequiresApi;
@@ -35,17 +40,30 @@ public class NLService extends NotificationListenerService {
         unregisterReceiver(nlservicereciver);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
 
         Log.i(TAG,"**********  onNotificationPosted");
 //        Log.i(TAG, java.text.DateFormat.getDateTimeInstance().format(new Date()) + " ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
         Log.i(TAG, java.text.DateFormat.getDateTimeInstance().format(new Date()) + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
-        Intent i = new  Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
-//        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "\n");
-        String to_post = "Posted: " + java.text.DateFormat.getDateTimeInstance().format(new Date()) + " " + sbn.getPackageName() + ": " + sbn.getNotification().tickerText + "\n";
-        i.putExtra("notification_event",to_post);
-        sendBroadcast(i);
+//        Intent i = new  Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
+////        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "\n");
+//        String to_post = "Posted: " + java.text.DateFormat.getDateTimeInstance().format(new Date()) + " " + sbn.getPackageName() + ": " + sbn.getNotification().tickerText + "\n";
+//        i.putExtra("notification_event",to_post);
+//        sendBroadcast(i);
+//
+//        Bundle extras = sbn.getNotification().extras;
+//
+//        if (extras.containsKey("android.text")) {
+//            if (extras.getCharSequence("android.text") != null) {
+//                String text = extras.getCharSequence("android.text").toString();
+//                Log.i(TAG, "------------------------- in onNotificationPosted(), Bundle.text != NULL, so here it is = " + text);
+//            }
+//        }
+//        if (extras.containsKey("android.title")) {
+//            Log.i(TAG, "------------------------- in onNotificationPosted(), Bundle android.title = " + extras.getString("android.title"));
+//        }
 
     }
 
@@ -54,36 +72,92 @@ public class NLService extends NotificationListenerService {
         Log.i(TAG,"********** onNOtificationRemoved");
 //        Log.i(TAG, System.currentTimeMillis() + "ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText +"\t" + sbn.getPackageName());
         Log.i(TAG, java.text.DateFormat.getDateTimeInstance().format(new Date()) + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
-        Intent i = new  Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
-//        i.putExtra("notification_event","onNotificationRemoved :" + sbn.getPackageName() + "\n");
-        String to_post = "Removed: " + java.text.DateFormat.getDateTimeInstance().format(new Date()) + " " + sbn.getPackageName() + ": " + sbn.getNotification().tickerText + "\n";
-        i.putExtra("notification_event",to_post);
-
-        sendBroadcast(i);
+//        Intent i = new  Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
+////        i.putExtra("notification_event","onNotificationRemoved :" + sbn.getPackageName() + "\n");
+//        String to_post = "Removed: " + java.text.DateFormat.getDateTimeInstance().format(new Date()) + " " + sbn.getPackageName() + ": " + sbn.getNotification().tickerText + "\n";
+////        i.putExtra("notification_event",to_post);
+//
+//        sendBroadcast(i);
     }
 
     class NLServiceReceiver extends BroadcastReceiver{
 
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         public void onReceive(Context context, Intent intent) {
             if(intent.getStringExtra("command").equals("clearall")){
                 NLService.this.cancelAllNotifications();
             }
             else if(intent.getStringExtra("command").equals("list")){
-                Intent i1 = new Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
-                i1.putExtra("notification_event","=====================");
-                sendBroadcast(i1);
+                List<String> noti_list = new ArrayList<String>();
+                String tmp = "=====================\n";
+                noti_list.add(tmp);
                 int i=1;
                 for (StatusBarNotification sbn : NLService.this.getActiveNotifications()) {
-                    Intent i2 = new  Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
-                    i2.putExtra("notification_event",i +" " + sbn.getPackageName() + "\n");
-                    sendBroadcast(i2);
-                    i++;
-                }
-                Intent i3 = new Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
-                i3.putExtra("notification_event","===== Notification List ====");
-                sendBroadcast(i3);
+                    tmp = i + "\n";
+                    SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+//                    Date date = new Date(System.currentTimeMillis());
+                    Date date = new Date(sbn.getPostTime());
+//                    System.out.println(formatter.format(date));
+//                    tmp = tmp + java.text.DateFormat.getDateTimeInstance().format(new Date()) + " " + sbn.getPackageName() + " " + sbn.getNotification().tickerText + "\n";
+//                    tmp = tmp + " " + formatter.format(date) + "\n";
+                    //now java.text.DateFormat.getDateTimeInstance().format(new Date())
+                    tmp = tmp + formatter.format(date) + " " + sbn.getPackageName() + " " + sbn.getNotification().tickerText + "\n";
+                    Bundle extras = sbn.getNotification().extras;
+                    if (extras.containsKey("android.title")) {
+                        tmp = tmp + "title " + extras.getString("android.title") + "\n";
+//                        Log.i(TAG, "------------------------- in onNotificationPosted(), Bundle android.title = " + extras.getString("android.title"));
+                        StringBuilder builder = new StringBuilder("Extras:\n");
+                        for (String key : extras.keySet()) { //extras is the Bundle containing info
+                            Object value = extras.get(key); //get the current object
+                            builder.append(key).append(": ").append(value).append("\n"); //add the key-value pair to the
+                        }
+                        Log.i("Extras",builder.toString()); //log the data or use it as needed.
+                    }
+                    if (extras.containsKey("android.text")) {
+                        if (extras.getCharSequence("android.text") != null) {
+                            String text = extras.getCharSequence("android.text").toString();
+//                            Log.i(TAG, "------------------------- in onNotificationPosted(), Bundle.text != NULL, so here it is = " + text);
+                            tmp = tmp + "text " + text + "\n";
+                        }
+                    }
 
+//                    tmp = tmp + "\n";
+                    i++;
+                    noti_list.add(tmp);
+                }
+                for (int index = noti_list.size()-1; index >=0; index--) {
+//                    System.out.println(crunchifyList.get(i));
+                    Intent i1 = new Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
+                    i1.putExtra("notification_event",noti_list.get(index));
+                    System.out.println(noti_list.get(index));
+                    sendBroadcast(i1);
+                }
+
+//                Intent i1 = new Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
+//                i1.putExtra("notification_event","=====================" + "\n");
+//                sendBroadcast(i1);
+//                int i=1;
+//                for (StatusBarNotification sbn : NLService.this.getActiveNotifications()) {
+//                    Intent i2 = new  Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
+//                    i2.putExtra("notification_event",i +" " + sbn.getPackageName() + "\n");
+////                    i2.putExtra("notification_event",i +" " + sbn.getPackageName());
+//                    sendBroadcast(i2);
+//                    i++;
+//                    Bundle extras = sbn.getNotification().extras;
+//                    if (extras.containsKey("android.text")) {
+//                        if (extras.getCharSequence("android.text") != null) {
+//                            String text = extras.getCharSequence("android.text").toString();
+//                            Log.i(TAG, "------------------------- in onNotificationPosted(), Bundle.text != NULL, so here it is = " + text);
+//                        }
+//                    }
+//                    if (extras.containsKey("android.title")) {
+//                        Log.i(TAG, "------------------------- in onNotificationPosted(), Bundle android.title = " + extras.getString("android.title"));
+//                    }
+//                }
+//                Intent i3 = new Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_EXAMPLE");
+//                i3.putExtra("notification_event","===== Notification List ====");
+//                sendBroadcast(i3);
             }
 
         }
--- app_i/src/main/java/com/recoveryrecord/surveyandroid/example/NotificationListActivity.java
+++ app_i/src/main/java/com/recoveryrecord/surveyandroid/example/NotificationListActivity.java
@@ -26,8 +26,8 @@ public class NotificationListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_list);
-//        Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-//        startActivity(intent);
+        Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+        startActivity(intent);
         txtView = (TextView) findViewById(R.id.textView);
         Button btnCreateNotify = (Button) findViewById(R.id.btnCreateNotify);
         Button btnClearNotify = (Button) findViewById(R.id.btnClearNotify);
@@ -76,55 +76,11 @@ public class NotificationListActivity extends Activity {
         unregisterReceiver(nReceiver);
     }
 
-
-
-//    public void buttonClicked(View v){
-//
-//        if(v.getId() == R.id.btnCreateNotify){
-////            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-//////            NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this);
-//////            ncomp.setContentTitle("My Notification");
-//////            ncomp.setContentText("Notification Listener Service Example");
-//////            ncomp.setTicker("Notification Listener Service Example");
-//////            ncomp.setSmallIcon(R.drawable.ic_launcher);
-//////            ncomp.setAutoCancel(true);
-//////            nManager.notify((int)System.currentTimeMillis(),ncomp.build());
-////            String NOTIFICATION_ID = "notification-id" ;
-////            String NOTIFICATION = "notification" ;
-//////            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context. NOTIFICATION_SERVICE ) ;
-//////            Notification notification = intent.getParcelableExtra( NOTIFICATION ) ;
-////            if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
-////                int importance = NotificationManager. IMPORTANCE_HIGH ;
-////                NotificationChannel notificationChannel = new NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
-////                assert nManager != null;
-////                nManager.createNotificationChannel(notificationChannel) ;
-////            }
-//////        int id = intent.getIntExtra( NOTIFICATION_ID , 0 ) ;
-//////        int id = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
-////            assert nManager != null;
-////            nManager.notify((int) System.currentTimeMillis(), ) ;
-//        }
-//        else if(v.getId() == R.id.btnClearNotify){
-//            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-//            nManager.cancelAll();
-//            Log.e("log: bluetooth device", "123131321");
-//            Intent i = new Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
-//            i.putExtra("command","clearall");
-//            sendBroadcast(i);
-//        }
-//        else if(v.getId() == R.id.btnListNotify){
-//            Intent i = new Intent("com.recoveryrecord.surveyandroid.example.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
-//            i.putExtra("command","list");
-//            sendBroadcast(i);
-//        }
-//
-//    }
-
     class NotificationReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            String temp = intent.getStringExtra("notification_event") + "n" + txtView.getText();
+            String temp = intent.getStringExtra("notification_event") + txtView.getText();
             txtView.setText(temp);
         }
     }
