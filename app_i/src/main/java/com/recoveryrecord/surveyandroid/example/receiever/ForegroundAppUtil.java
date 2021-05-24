23cb962da6609a7c31bcb40cb8baa3d6b0d01766
laiyousin <c611611c@gmail.com>
2021-05-24 12:39:31 +0800

    21.05.23  Add Stream v3

--- app_i/src/main/java/com/recoveryrecord/surveyandroid/example/receiever/ForegroundAppUtil.java
+++ app_i/src/main/java/com/recoveryrecord/surveyandroid/example/receiever/ForegroundAppUtil.java
@@ -89,7 +89,7 @@ public class ForegroundAppUtil{
 
             List<UsageStats> usageStatses = manager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, startTime, endTime);
             if (usageStatses == null || usageStatses.size() == 0) {//沒有權限無法獲取app
-                Toast.makeText(context, "勾選允許存取「使用量存取資料」", Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "請勾選允許存取「使用量存取資料」", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.getApplicationContext().startActivity(intent);
