856851095ad705808584c9bfbde5c16303a596a2
wumamu <wcclevi1225@gmail.com>
2023-10-07 01:45:52 +0800

    add scroll more

--- app_i/src/main/java/com/recoveryrecord/surveyandroid/example/ui/NewsSubFragment.kt
+++ app_i/src/main/java/com/recoveryrecord/surveyandroid/example/ui/NewsSubFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.recoveryrecord.surveyandroid.example.Constants
@@ -20,13 +22,16 @@ const val NEWS_SOURCE = "news_source"
 const val NEWS_CATEGORY = "news_category"
 
 class NewsSubFragment : Fragment() {
-    private var courseRV: RecyclerView? = null
-    private var dataModalArrayList: ArrayList<News>? = null
-    private var dataRVAdapter: NewsRecycleViewAdapter? = null
-    private var db: FirebaseFirestore? = null
+    private lateinit var courseRV: RecyclerView
+    private lateinit var fabScrollToTop: FloatingActionButton
+    private val dataModalArrayList: ArrayList<News> = ArrayList()
+    private lateinit var dataRVAdapter: NewsRecycleViewAdapter
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
     private var source: String = ""
     private var category: String = ""
-//    private val viewModel by viewModel()
+    private val pageSize: Long = Constants.NEWS_LIMIT_PER_PAGE.toLong()
+    private var lastVisibleDocument: DocumentSnapshot? = null
+    private var isFetchingData: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -42,47 +47,63 @@ class NewsSubFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.nested_layer2_category, container, false)
         courseRV = view.findViewById(R.id.idRVItems)
+        fabScrollToTop = view.findViewById(R.id.fab)
+        fabScrollToTop.hide()
+
+        initRecyclerView()
+        fetchInitialData()
 
 
-        db = FirebaseFirestore.getInstance()
-
-        dataModalArrayList = ArrayList<News>()
-        courseRV?.apply {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(
-                activity,
-                LinearLayoutManager.VERTICAL,
-                false
-            )
+        fabScrollToTop.setOnClickListener {
+            // Scroll to the top of the RecyclerView
+            courseRV.scrollToPosition(0)
         }
 
-        // adding our array list to our recycler view adapter class.
-        dataRVAdapter = activity?.let { NewsRecycleViewAdapter(dataModalArrayList!!, it) }
+        // Add a scroll listener to implement pagination
+        courseRV.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val visibleItemCount = layoutManager.childCount
+                val totalItemCount = layoutManager.itemCount
+                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
 
-        // setting adapter to our recycler view.
-        courseRV?.apply { adapter = dataRVAdapter }
-        if (source == "storm") {
-            fetchNewsByContain(source, category)
-        } else {
-            fetchNewsByEqual(source, category)
-        }
+                if (!isFetchingData && lastVisibleDocument != null) {
+                    if (visibleItemCount + firstVisibleItemPosition >= totalItemCount
+                        && firstVisibleItemPosition >= 0
+                    ) {
+                        fetchMoreData()
+                    }
+                }
+                if (firstVisibleItemPosition > 0) {
+                    fabScrollToTop.show()
+                } else {
+                    fabScrollToTop.hide()
+                }
+            }
+        })
 
         return view
     }
 
-    @SuppressLint("NotifyDataSetChanged")
-    private fun fetchNewsByContain(source: String, category: String) {
-        db!!.collection("news")
-            .whereEqualTo("media", source)
-            .whereArrayContains("category", category)
-            .orderBy("pubdate", Query.Direction.DESCENDING)
-            .limit(Constants.NEWS_LIMIT_PER_PAGE.toLong())
+    private fun initRecyclerView() {
+        courseRV.setHasFixedSize(true)
+        courseRV.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        dataRVAdapter = NewsRecycleViewAdapter(dataModalArrayList, requireActivity())
+        courseRV.adapter = dataRVAdapter
+    }
+
+    private fun fetchInitialData() {
+        // Fetch the initial data
+        val query = createQuery()
+        query.limit(pageSize)
             .get()
             .addOnSuccessListener { queryDocumentSnapshots ->
-                Log.d("logpager", "sss")
                 if (!queryDocumentSnapshots.isEmpty) {
                     val list = queryDocumentSnapshots.documents
-                    for (d in list) {;
+                    lastVisibleDocument = queryDocumentSnapshots.documents.lastOrNull()
+
+                    for (d in list) {
                         val dataModal = News(
                             title = d.getString("title"),
                             media = d.getString("media"),
@@ -90,28 +111,44 @@ class NewsSubFragment : Fragment() {
                             pubDate = d.getTimestamp("pubdate"),
                             image = d.getString("image")
                         )
-                        dataModalArrayList?.add(dataModal)
+                        dataModalArrayList.add(dataModal)
                     }
-                    dataRVAdapter!!.notifyDataSetChanged()
+                    dataRVAdapter.notifyDataSetChanged()
                 }
-            }.addOnFailureListener { e ->
+            }
+            .addOnFailureListener { e ->
                 Log.d("logpager", "Fail to get the data.$e")
             }
     }
 
+    private fun createQuery(): Query {
+        return if (source == "storm") {
+            db.collection("news")
+                .whereEqualTo("media", source)
+                .whereArrayContains("category", category)
+                .orderBy("pubdate", Query.Direction.DESCENDING)
+        } else {
+            db.collection("news")
+                .whereEqualTo("media", source)
+                .whereEqualTo("category", category)
+                .orderBy("pubdate", Query.Direction.DESCENDING)
+        }
+    }
+
     @SuppressLint("NotifyDataSetChanged")
-    private fun fetchNewsByEqual(source: String, category: String) {
-        db!!.collection("news")
-            .whereEqualTo("media", source)
-            .whereEqualTo("category", category)
-            .orderBy("pubdate", Query.Direction.DESCENDING)
-            .limit(Constants.NEWS_LIMIT_PER_PAGE.toLong())
+    private fun fetchMoreData() {
+        // Fetch more data using the last visible document as a starting point
+        val query = createQuery()
+        query
+            .startAfter(lastVisibleDocument)
+            .limit(pageSize)
             .get()
             .addOnSuccessListener { queryDocumentSnapshots ->
-                Log.d("logpager", "sss")
                 if (!queryDocumentSnapshots.isEmpty) {
                     val list = queryDocumentSnapshots.documents
-                    for (d in list) {;
+                    lastVisibleDocument = queryDocumentSnapshots.documents.lastOrNull()
+
+                    for (d in list) {
                         val dataModal = News(
                             title = d.getString("title"),
                             media = d.getString("media"),
@@ -119,12 +156,15 @@ class NewsSubFragment : Fragment() {
                             pubDate = d.getTimestamp("pubdate"),
                             image = d.getString("image")
                         )
-                        dataModalArrayList?.add(dataModal)
+                        dataModalArrayList.add(dataModal)
                     }
-                    dataRVAdapter!!.notifyDataSetChanged()
+                    dataRVAdapter.notifyDataSetChanged()
                 }
-            }.addOnFailureListener { e ->
-                Log.d("logpager", "Fail to get the data.$e")
+                isFetchingData = false
+            }
+            .addOnFailureListener { e ->
+                Log.d("logpager", "Fail to get more data.$e")
+                isFetchingData = false
             }
     }
 
